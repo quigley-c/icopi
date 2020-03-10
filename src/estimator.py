8717bec3c965c6da8306c5541cf0045f7fc2a3c2
quigley-c <quigleyc@allegheny.edu>
2020-03-10 03:04:42 +0000

    updates to estimator

--- src/estimator.py
+++ src/estimator.py
@@ -2,16 +2,40 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import tensorflow as tf
 import IPython.display as display
-from PIL import Image
+import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import os, pathlib
+import matplotlib as plt
+import os, sys
 
 from prepare_data import *
 
+
+NUMERIC_COLUMNS = ['red', 'green', 'blue']
+
 def estimate_palettes():
     print("estimating colors")
+    
+    feature_columns = []
+
+    #generate the feature_columns
+    for feature_name in NUMERIC_COLUMNS:
+        feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
+
+    #get dataset from prepare_data as a tf.data.Dataset
+    dataset = prepare_data()
+
+    train_input_fn
+
+    #set up the linear estimator for the dataset
+    linear_est = tf.estimator.LinearClassifier(feature_columns=featurecolumns)
+    linear_est.train(train_input_fn)
+    result = linear_est.evaluate(eval_input_fn)
+
+    clear_output()
+    print(result)
 #
+
+def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True,
 
 if __name__ == "__main__":
     estimate_palettes()
