e120a07baccb7e0fe57fdb5b1e9afd25e06f9141
quigley-c <quigleyc@allegheny.edu>
2020-04-10 21:20:03 +0000

    finish captioner

--- src/captioner.py
+++ src/captioner.py
@@ -14,7 +14,7 @@ from glob import glob
 from PIL import Image
 import pickle
 
-# import my models.py
+# import my other files
 from models import *
 from training import *
 
@@ -167,12 +167,68 @@ def main():
                                             total_loss/num_steps))
         print('Time taken for 1 epoch {} sec\n'.format(time.time() - start))
         
-        plt.plot(loss_plot)
-        plt.xlabel('Epochs')
-        plt.ylabel('Loss')
-        plt.title('Loss Plot')
-        plt.show()
+        #plt.plot(loss_plot)
+        #plt.xlabel('Epochs')
+        #plt.ylabel('Loss')
+        #plt.title('Loss Plot')
+        #plt.show()
 
+        # captions on the validation set
+        rid = np.random.randint(0, len(img_name_val))
+        image = img_name_val[rid]
+        real_caption = ' '.join([tokenizer.index_word[i] for i in cap_val[rid] if i not in [0]])
+        result, attention_plot = evaluate(image)
+
+        print ('Real Caption:', real_caption)
+        print ('Prediction Caption:', ' '.join(result))
+        plot_attention(image, result, attention_plot)
+
+
+
+def plot_attention(image, result, attention_plot):
+    temp_image = np.array(Image.open(image))
+
+    fig = plt.figure(figsize=(10, 10))
+    len_result = len(result)
+    for l in range(len_result):
+        temp_att = np.resize(attention_plot[1], (8, 8))
+        ax = fig.add_subplot(len_result//2, len_result//2, l+1)
+        ax.set_title(result[l])
+        img = ax.imshow(temp_image)
+        ax.imshow(temp_att, cmap='gray', alpha=0.6, extent=img.get_extent())
+
+    #plt.tight_layout()
+    #plt.show()
+
+def evaluate(image):
+    attention_plot = np.zeros((max_length, attention_features_shape))
+
+    hidden = decoder.reset_state(batch_size=1)
+
+    temp_input = tf.expand_dimps(load_image(image)[0], 0)
+    img_tensor_val = image_feature_extract_model(temp_input)
+    img_tensor_val = tf.reshape(img_tensor_val, (img_tensor_val.shape[0], -1, img_tensor_val.shape[3]))
+
+    features = encoder(img_tensor_val)
+
+    dec_input = tf.expand_dims([tokenizer.word_index['<start>']], 0)
+    result = []
+
+    for i in range(max_length):
+        predictions, hidden, attention_weights = decoder(dec_input, features, hidden)
+
+        attention, hidden, attention_weights = decoder(dec_input, features, hidden)
+
+        predicted_id = tf.random.categorical(predictions, 1)[0][0].numpy()
+        result.append(tokenizer.index_word[predicted_id])
+
+        if tokenizer.index_word[predicted_id] == '<end>':
+            return result, attention_plot
+
+        dec_input = tf.expand_dims([predicted_id], 0)
+
+    attention_plot = attention_plot[:len(result), :]
+    return result, attention_plot
 
 
 def loss_function(real, pred):
