67368fa0bcaff267bfe0906723b04fc6a30f907a
quigley-c <quigleyc@allegheny.edu>
2020-04-10 18:32:15 +0000

    fix features not generating for greyscale images

--- src/generate_features.py
+++ src/generate_features.py
@@ -10,24 +10,32 @@ for img in os.listdir('..' + img_path):
     
     print('image', img, ' shape:', image.shape)
 
-    IMG_WIDTH = image.shape[0]
-    IMG_HEIGHT = image.shape[1]
+    IMG_HEIGHT = image.shape[0]
+    IMG_WIDTH = image.shape[1]
 
     # create 2d feature matrix for image width, height
-    feature_matrix = np.zeros((IMG_WIDTH, IMG_HEIGHT))  
+    feature_matrix = np.zeros((IMG_HEIGHT, IMG_WIDTH))  
     print('matrix shape:', feature_matrix.shape)
 
     # populate feature matrix with the 3 color channels average values
-    for i in range(0, IMG_WIDTH):
-        for j in range(0, IMG_HEIGHT):
-            feature_matrix[i][j] = ((int(image[i,j,0]) + int(image[i,j,1]) + int(image[i,j,2]))/3)
+    for i in range(0, IMG_HEIGHT):
+        for j in range(0, IMG_WIDTH):
+            
+            # if the image is grayscale it won't contain color channel data
+            if len(image.shape) == 2:
+                feature_matrix[i][j] = ((int(image[i,j])))
+            else:
+                feature_matrix[i][j] = ((int(image[i,j,0]) +
+                                        int(image[i,j,1]) +
+                                        int(image[i,j,2])) / 
+                                        image.shape[2])
 
     # reshape to 1d array
-    features = np.reshape(feature_matrix, (IMG_WIDTH*IMG_HEIGHT))
+    features = np.reshape(feature_matrix, (IMG_HEIGHT*IMG_WIDTH))
     print("features shape:", features.shape)
 
     npy_name = img + '.npy'
-    npy_path = '..' + img_path + npy_name
+    npy_path = '..' + '/data/training/npy/' + npy_name
     print('saving to:', npy_path)
 
     # save features to npy file
